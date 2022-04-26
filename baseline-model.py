4820dc270a7da7d02f22f72f84fe5b16b34712c5
KunalSinha1125 <ksinha2@stanford.edu>
2022-04-25 17:54:10 -0700

    Rank images based on similarity to text

--- baseline-model.py
+++ baseline-model.py
@@ -1,3 +1,4 @@
+import os
 import torch
 import clip
 from PIL import Image
@@ -6,23 +7,30 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 model, preprocess = clip.load("ViT-B/32", device=device)
 
 def main():
-    image_name = 'cs permutations.png'
-    text_names = ['a diagram', 'a dog', 'a cat']
-    probs = run_clip(image_name, text_names)
-    print(probs)
+    image_inputs = ['diagram.png', 'fish.png']
+    text_input = 'fish'
+    run_clip(image_inputs, text_input)
 
-def run_clip(image_name, text_names):
+def run_clip(image_inputs, text_input, top_k=2):
 
-    image = preprocess(Image.open(image_name)).unsqueeze(0).to(device)
-    text = clip.tokenize(text_names).to(device)
-
+    images = torch.cat( #Create a tensor representation for the images
+        [preprocess(Image.open(img)).unsqueeze(0).to(device) for img in image_inputs]
+    ).to(device)
+    text = clip.tokenize(text_input).to(device) #Create a tensor representation for the text
     with torch.no_grad():
-        image_features = model.encode_image(image)
-        text_features = model.encode_text(text)
+        image_features = model.encode_image(images) #Produce image embeddings
+        text_features = model.encode_text(text) #Produce text embeddings
 
-        logits_per_image, logits_per_text = model(image, text)
-        probs = logits_per_image.softmax(dim=-1).cpu().numpy()
-        return probs
+    # Pick the top 5 most similar labels for the image
+    image_features /= image_features.norm(dim=-1, keepdim=True)
+    text_features /= text_features.norm(dim=-1, keepdim=True)
+    similarity = (100.0 * text_features @ image_features.T).softmax(dim=-1)
+    values, indices = similarity[0].topk(top_k)
+
+    # Print the result
+    print("\nTop predictions:\n")
+    for value, index in zip(values, indices):
+        print(f"{image_inputs[index]:>16s}: {100 * value.item():.2f}%")
 
 if __name__ == "__main__":
     main()
